1f0fa8cbd8149bd001244457c4109b7a71a7c7f8
Anirban Dey <116617392+anirbandey1@users.noreply.github.com>
2023-07-29 11:57:12 +0530

    Playstore icon added to Top Bar

---
+++ src/components/frontend.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import {TopBar} from "./topbar.jsx"
+import "./style.css"
+const FrontEnd = ()=> {
+
+  return (
+    <>
+      <TopBar />
+      <div>
+        
+      </div>
+
+    </>
+
+
+  )
+
+
+}
+
+
+
+export {FrontEnd}
---
+++ src/components/topbar.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import gps from "../assets/googleplaystore.png"
+const TopBar = ()=> {
+
+  return (
+    <>
+        <div className="topbar">
+            <img src={gps} className="googleplaystore-topbar-icon"/>
+
+            <div className="options">
+              <div className="option-item">
+                Games
+              </div>
+         
+              <div className="option-item">
+                Apps
+              </div>
+
+              <div className="option-item">
+                Movies
+              </div>
+
+              <div className="option-item">
+                Books
+              </div>
+              <div className="option-item">
+                Kids
+              </div>
+            </div>
+        
+        </div>
+
+    </>
+  )
+
+}
+
+
+export {TopBar}
